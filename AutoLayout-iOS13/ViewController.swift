a15392e24848ff1946b7543f49327b19bec40a3e
james stark <james.starkg@gmail.com>
2023-01-07 14:41:54 +1100

    Finish the task

--- AutoLayout-iOS13/ViewController.swift
+++ AutoLayout-iOS13/ViewController.swift
@@ -13,10 +13,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
+    let allDice = [UIImage(imageLiteralResourceName: "DiceOne"),
+                   UIImage(imageLiteralResourceName: "DiceTwo"),
+                   UIImage(imageLiteralResourceName: "DiceThree"),
+                   UIImage(imageLiteralResourceName: "DiceFour"),
+                   UIImage(imageLiteralResourceName: "DiceFive"),
+                   UIImage(imageLiteralResourceName: "DiceSix")]
+    
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        
-        let allDice = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
-        
         diceImageView1.image = allDice[Int.random(in: 0...5)]
         diceImageView2.image = allDice[Int.random(in: 0...5)]
         
